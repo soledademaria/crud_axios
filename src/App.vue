263ff90c6604b4d57cc7d6f5a01261ba63f5db40
soledademaria <solmmaria50@gmail.com>
2022-05-18 21:24:34 -0300

    create form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <template>
 	<div id="app" class="container">
+		<b-card>
+			<b-form-group label="Nome: ">
+				<b-form-input
+				v-model="user.name"
+				type="text"
+				size="lg"
+				placeholder="Digite o seu nome"
+				></b-form-input>
+			</b-form-group>
+
+			<b-form-group label="Email: ">
+				<b-form-input
+				v-model="user.email"
+				type="email"
+				size="lg"
+				placeholder="Digite o seu email"
+				></b-form-input>
+			</b-form-group>
+			<hr>
+
+			<b-button size="lg" variant="primary" @click="handleClick">Salvar</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			user: {
+				name: '',
+				email: ''
+			}
+		}
+	},
+
+	methods: {
+		handleClick(){
+			console.log(this.user);
+		}
+	}
 }
 </script>
 
